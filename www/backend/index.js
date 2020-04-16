aa6fa55821b2fe966bdbaa005de7c7b61415936c
Gian Eric <gianeric08@gmail.com>
2020-04-16 01:04:37 -0300

    [vsCode]const params = request.query

--- www/backend/index.js
+++ www/backend/index.js
@@ -1,13 +1,36 @@
 const express = require ('express'); //constante express recebe todos os dados de do framework express
 const app = express(); //instanciar constante express
+/* 
+  Rotas / Recursos
+*/
+/*
+  Métodos HTTP:
 
-app.get('/', (request,response) => {
+  GET: Buscar/Listar uma informação no Back End   
+  POST: Criar uma informação no Back End
+  PUT: Alterar uma informação no Back End
+  DELETE: Deletar uma informação no Back end
+*/
+
+/**
+ * Tipos de parâmetros
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após  "?" (Filtros, paginação) Exemplo: "http://localhost:3333/users?name=Gian&idade=26"
+ * Route Params: Parâmetros utilizados para identificar recursos Exemplo"http://localhost:3333/users/:id"
+ * Request Body: 
+ */
+
+app.get('/users', (request,response) => {
+  const params = request.query;
+
+  console.log(params);
+
   //return response.send("Hello Word");
   return response.json({
       evento: 'Semana OmniStack',
       aluno: 'Gian Eric' 
 })
-}); 
+});
 
 app.listen(3333); //ouvir a porta 3333
 
