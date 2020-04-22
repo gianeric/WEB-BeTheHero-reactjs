4e9cc8c05eb15e71398f34f6dbc23bac9a48b233
Gian Eric <gianeric08@gmail.com>
2020-04-21 22:44:44 -0300

    [vsCode]criação da conexão com o banco de dados
    Knex -> Connection -> rotas

--- www/backend/src/routes.js
+++ www/backend/src/routes.js
@@ -1,16 +1,30 @@
-const express = require('express'); //constante express recebe todos os dados de do framework express
+const express    = require('express'); //constante express recebe todos os dados de do framework express
+const crypto     = require('crypto');
+const connection = require('./database/connection');
+
 const routes = express.Router();
 
-routes.post('/users', (request,response) => {
-    const body = request.body;
+routes.get('/ongs', async(request, response) => {
+  const ongs = await connection('ongs').select('*');
   
-    console.log(body);
-  
-    //return response.send("Hello Word");
-    return response.json({
-        evento: 'Semana OmniStack',
-        aluno: 'Gian Eric' 
-  })
+  return response.json(ongs)
+});
+
+routes.post('/ongs', async(request,response) => {
+    const {name, email, whatsapp, city, uf} = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX'); //Criptografia de ID
+
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    })
+    
+    return response.json({ id });
   });
 
 module.exports = routes;
