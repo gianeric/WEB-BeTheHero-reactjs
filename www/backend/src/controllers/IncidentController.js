4943087f0faf79c9de15f0ec20978292fee2fd17
Gian Eric <gianeric08@gmail.com>
2020-04-28 17:08:52 -0300

    [vsCode]controllers > IncidentController

---
+++ www/backend/src/controllers/IncidentController.js
@@ -0,0 +1,24 @@
+//const crypto     = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+     async index(request, response) {
+      const incidents = await connection('incidents').select('*');
+
+      return response.json(incidents);
+    },
+
+    async create(request, response) {
+      const {title, description, value} = request.body;
+      const ong_id =  request.headers.authorization; //Localiza o id do Cabeçalho
+
+      const [id] = await connection('incidents').insert({
+        title,
+        description,
+        value,
+        ong_id,
+      })
+
+      return response.json({ id });    
+    },
+};
--- www/backend/src/routes.js
+++ www/backend/src/routes.js
@@ -1,9 +1,13 @@
-const express       = require('express'); //constante express recebe todos os dados de do framework express
-const OngController = require('./controllers/OngController');
+const express            = require('express'); //constante express recebe todos os dados de do framework express
+const OngController      = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes;
