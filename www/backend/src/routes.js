78b10f41c84483af49a060d742efb4d5ad212e48
Gian Eric <gianeric08@gmail.com>
2020-04-29 23:28:55 -0300

    [vsCode]controllers > ProfilerController

--- www/backend/src/routes.js
+++ www/backend/src/routes.js
@@ -1,6 +1,7 @@
 const express            = require('express'); //constante express recebe todos os dados de do framework express
 const OngController      = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfilerController = require('./controllers/ProfilerController');
 
 const routes = express.Router();
 
@@ -10,5 +11,7 @@ routes.post('/ongs', OngController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfilerController.index);
 
 module.exports = routes;
